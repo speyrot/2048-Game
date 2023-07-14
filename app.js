090b612a06384eefe1ec018da3044c4f91994fd1
TKYOpartner <128873901+TKYOpartner@users.noreply.github.com>
2023-07-14 19:27:07 -0400

    after a long day finally finished and debugged

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+//link components
+
 let grid = document.querySelector(".grid");
 
-const startButton = document.getElementById(".container");
+const startButton = document.getElementById("start-button");
+
+const container = document.querySelector(".container");
 
 const coverScreen = document.querySelector(".cover-screen");
 
 const result = document.getElementById("result");
 
 const overText = document.getElementById("over-text");
+
+
+//Basic setups
 
 let matrix,
     score, 
@@ -43,6 +50,9 @@ const createGrid = () => {
     }
 };
 
+
+//Move checking logic
+
 const adjacentCheck = (arr) => {
     for(let i = 0; i < arr.length - 1; i++) {
         if(arr[i] == arr[i + 1]) {
@@ -50,15 +60,6 @@ const adjacentCheck = (arr) => {
         }
     }
 
-    return false;
-};
-
-const adjacentCheck = (arr) => {
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] == arr[i + 1]) {
-        return true;
-      }
-    }
     return false;
 };
 
@@ -80,6 +81,9 @@ const possibleMovesCheck = () => {
     }
     return false;
 };
+
+
+// Game logic
 
 const randomPosition = (arr) => {
     return Math.floor(Math.random() * arr.length);
@@ -105,3 +109,306 @@ const gameOverCheck = () => {
       startButton.innerText = "Restart Game";
     }
 };
+
+const generateTwo = () => {
+    if (hasEmptyBox()) {
+
+        let randomRow = randomPosition(matrix);
+
+        let randomCol = randomPosition(matrix[randomPosition(matrix)]);
+
+        if (matrix[randomRow][randomCol] == 0) {
+            
+            matrix[randomRow][randomCol] = 2;
+
+            let element = document.querySelector(`[data-position = '${randomRow}_${randomCol}']`);
+
+            element.innerHTML = 2;
+
+            element.classList.add("box-2");
+
+        } else {
+            generateTwo()
+        }
+
+    } else {
+        gameOverCheck();
+    }
+};
+
+const generateFour = () => {
+    if (hasEmptyBox()) {
+
+        let randomRow = randomPosition(matrix);
+
+        let randomCol = randomPosition(matrix[randomPosition(matrix)]);
+
+        if (matrix[randomRow][randomCol] == 0) {
+            
+            matrix[randomRow][randomCol] = 4;
+
+            let element = document.querySelector(`[data-position = '${randomRow}_${randomCol}']`);
+
+            element.innerHTML = 4;
+
+            element.classList.add("box-4");
+
+        } else {
+            generateFour()
+        }
+
+    } else {
+        gameOverCheck();
+    }
+};
+
+const removeZero = (arr) => arr.filter((num) => num);
+
+const checker = (arr, reverseArr = false) => {
+    arr = reverseArr ? removeZero(arr).reverse() : removeZero(arr);
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] == arr[i + 1]) {
+            arr[i] += arr[i + 1];
+            arr[i + 1] = 0;
+            score += arr[i]; 
+        }
+    }
+
+    arr = reverseArr ? removeZero(arr).reverse() : removeZero(arr);
+
+    let missingCount = 4 - arr.length;
+
+    while (missingCount > 0) {
+        
+        if (reverseArr) {
+            arr.unshift(0);
+        } else {
+            arr.push(0);
+        }
+
+        missingCount -= 1;
+
+    }
+
+    return arr;
+
+};
+
+
+//Sliding movement
+
+const slideDown = () => {
+
+    for (let i = 0; i < columns; i++) {
+
+        let num = [];
+
+        for (let j = 0; j < rows; j++) {
+            
+            num.push(matrix[j][i])
+        
+        }
+
+        num = checker(num, true);
+
+        for (let j=0; j < rows; j++) {
+            
+            matrix[j][i] = num[j];
+
+            let element = document.querySelector(`[data-position='${j}_${i}']`);
+
+            element.innerHTML = matrix[j][i] ? matrix[j][i] : "";
+
+            element.classList.value = "";
+
+            element.classList.add("box", `box-${matrix[j][i]}`);
+
+        }
+    }
+
+    let decision = Math.random() > 0.5 ? 1 : 0;
+
+    if (decision) {
+
+        setTimeout(generateFour, 200);
+
+    } else {
+
+        setTimeout(generateTwo, 200);
+
+    }
+};
+
+const slideUp = () => {
+    for (let i = 0; i < columns; i++) {
+      let num = [];
+      for (let j = 0; j < rows; j++) {
+        num.push(matrix[j][i]);
+      }
+      num = checker(num);
+      for (let j = 0; j < rows; j++) {
+        matrix[j][i] = num[j];
+        let element = document.querySelector(`[data-position = '${j}_${i}']`);
+        element.innerHTML = matrix[j][i] ? matrix[j][i] : "";
+        element.classList.value = "";
+        element.classList.add("box", `box-${matrix[j][i]}`);
+      }
+    }
+    let decision = Math.random() > 0.5 ? 1 : 0;
+    if (decision) {
+      setTimeout(generateFour, 200);
+    } else {
+      setTimeout(generateTwo, 200);
+    }
+  };
+
+  const slideRight = () => {
+    for (let i = 0; i < rows; i++) {
+      let num = [];
+      for (let j = 0; j < columns; j++) {
+        num.push(matrix[i][j]);
+      }
+      num = checker(num, true);
+      for (let j = 0; j < columns; j++) {
+        matrix[i][j] = num[j];
+        let element = document.querySelector(`[data-position = '${i}_${j}']`);
+        element.innerHTML = matrix[i][j] ? matrix[i][j] : "";
+        element.classList.value = "";
+        element.classList.add("box", `box-${matrix[i][j]}`);
+      }
+    }
+    let decision = Math.random() > 0.5 ? 1 : 0;
+    if (decision) {
+      setTimeout(generateFour, 200);
+    } else {
+      setTimeout(generateTwo, 200);
+    }
+  };
+
+  const slideLeft = () => {
+    for (let i = 0; i < rows; i++) {
+      let num = [];
+      for (let j = 0; j < columns; j++) {
+        num.push(matrix[i][j]);
+      }
+      num = checker(num);
+      for (let j = 0; j < columns; j++) {
+        matrix[i][j] = num[j];
+        let element = document.querySelector(`[data-position = '${i}_${j}']`);
+        element.innerHTML = matrix[i][j] ? matrix[i][j] : "";
+        element.classList.value = "";
+        element.classList.add("box", `box-${matrix[i][j]}`);
+      }
+    }
+    let decision = Math.random() > 0.5 ? 1 : 0;
+    if (decision) {
+      setTimeout(generateFour, 200);
+    } else {
+      setTimeout(generateTwo, 200);
+    }
+  };
+
+
+  //Movement Listeners
+
+document.addEventListener("keyup", (e) => {
+    
+    if (e.code == "ArrowLeft") {
+        
+        slideLeft();
+    
+    } else if (e.code == "ArrowRight") {
+
+        slideRight();
+
+    } else if (e.code == "ArrowUp") {
+        
+        slideUp();
+
+    } else if (e.code == "ArrowDown") {
+
+        slideDown();
+
+    }
+
+    document.getElementById("score").innerText = score;
+
+});
+
+
+//Grid Listeners
+
+grid.addEventListener("touchstart", (event) => {
+
+    isSwiped = true;
+    
+    getXY(event);
+
+    initialX = touchX;
+
+    initialY = touchY;
+
+});
+
+grid.addEventListener("touchmove", (event) => {
+
+    if (isSwiped) {
+
+        getXY(event);
+
+        let diffX = touchX - initialX;
+
+        let diffY = touchY - initialY;
+
+        if (Math.abs(diffY) > Math.abs(diffX)) {
+            
+            swipeDirection = diffX > 0 ? "down" : "up";
+
+        } else {
+
+            swipeDirection = diffY > 0 ? "right" : "left";
+
+        }
+    }
+});
+
+
+//Start Function
+
+const startGame = () => {
+
+    score = 0;
+
+    document.getElementById("score").innerText = score;
+
+    grid.innerHTML = "";
+
+    matrix = [
+        
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+
+    ];
+
+    container.classList.remove("hide");
+
+    coverScreen.classList.add("hide");
+
+    createGrid();
+
+    generateTwo();
+
+    generateTwo();
+
+};
+
+startButton.addEventListener("click", () => {
+
+    startGame();
+    
+    swipeDirection = "";
+  
+});
